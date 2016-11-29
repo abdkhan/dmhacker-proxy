fd72f06a7af2ba16c373c4faf2702b19ef395da3
dmhacker <dmhacker@yahoo.com>
2016-11-28 20:14:58 -0800

    Remove old packages

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-/*
-var http = require('http'),
-    httpProxy = require('http-proxy');
-//
-// Create your proxy server and set the target in the options.
-//
-httpProxy.createProxyServer({
-    target: 'http://localhost:'+app.get('port')
-}).listen(8000);
-
-//
-// Create your target server
-//
-http.createServer(function(req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-    res.end();
-}).listen(app.get('port'));
-*/
-
 var express = require('express');
 var proxy = require('express-http-proxy');
 var urlExists = require('url-exists');
