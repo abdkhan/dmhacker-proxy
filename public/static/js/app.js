7c3df7df2bb2bd2d6d81e4bd2f24ce1649618cd4
dmhacker <dmhacker@yahoo.com>
2016-11-30 12:20:26 -0800

    Test

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -2,19 +2,24 @@ var app = angular.module('Proxy', []);
 
 app.controller('ProxyController', function($scope, $http) {
 
-    $scope.title = 'Bush did 7/11.';
     $http({
         method: 'GET',
         url: '/ip'
-    }).then(function (response) {
+    }).then(function(response) {
+        console.log(response);
         $scope.title = response.ip;
-        $scope.$digest();
-    }, function (err) {
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    }, function(err) {
         Materialize.toast('Unable to get server ip', 3000);
-        $scope.$digest();
+        $scope.title = 'Bush did 7/11.';
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
     });
 
-    $scope.go = function () {
+    $scope.go = function() {
         var b64url = window.btoa($scope.url);
         window.location.href = '/site/' + b64url;
     };
