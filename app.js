cf58980c6a426f59ba0c42977151c83e4d423c69
dmhacker <dmhacker@yahoo.com>
2016-11-30 06:22:47 -0800

    Wikipedia' case

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/site/:b64url', function(req, res) {
                             // We don't know what this is ... could be a link missing the http OR a call to the current directory
                             // Nevertheless, we make our best guess
                             var first_part = old_attr.split('/')[0];
-                            if (first_part.includes('.com') || first_part.includes('.org') || first_part.includes('.net') || first_part.includes('.edu')) {
+                            if (old_attr.substring(0, 2) === '//' || first_part.includes('.com') || first_part.includes('.org') || first_part.includes('.net') || first_part.includes('.edu')) {
                                 old_attr = urlObject.protocol + (urlObject.slashes ? '//' : '') + old_attr;
                             }
                             else {
