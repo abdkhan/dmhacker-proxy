879377a577322965c711ad6160cd4221a4c6961f
dmhacker <dmhacker@yahoo.com>
2016-11-30 11:10:51 -0800

    srcset

--- app.js
+++ app.js
@@ -38,36 +38,58 @@ app.get('/site/:b64url', function(req, res) {
                     ['link', 'href'],
                     ['a', 'href'],
                     ['script', 'src'],
-                    ['img', 'src']
+                    ['img', 'src'],
+                    ['img', 'srcset']
                 ];
+
+                var transformLink = function(old_attr) {
+                    old_attr = old_attr === undefined ? undefined : old_attr.trim();
+                    if (old_attr === undefined || old_attr[0] === '#') {
+                        return old_attr;
+                    }
+                    else if (old_attr[0] === '/') {
+                        if (old_attr.substring(0, 2) === '//') {
+                            old_attr = 'http:' + old_attr;
+                        }
+                        else {
+                            old_attr = urlObject.protocol + (urlObject.slashes ? '//' : '') + urlObject.hostname + (old_attr[0] === '/' ? '' : '/') + old_attr;
+                        }
+                    }
+                    else if (old_attr.substring(0, 4) !== 'http') {
+                        // We don't know what this is ... could be a link missing the http OR a call to the current directory
+                        // Nevertheless, we make our best guess
+                        var first_part = old_attr.split('/')[0];
+                        if (first_part.includes('.com') || first_part.includes('.org') || first_part.includes('.net') || first_part.includes('.edu')) {
+                            old_attr = urlObject.protocol + (urlObject.slashes ? '//' : '') + old_attr;
+                        }
+                        else {
+                            old_attr = urlLink + '/' + old_attr;
+                        }
+                    }
+                    var old_b64 = new Buffer(old_attr).toString('base64');
+                    return 'http://dmhacker-proxy.herokuapp.com/site/' + old_b64;
+                };
+
                 for (var t in targets) {
                     var target = targets[t];
                     $(target[0]).each(function () {
                         var old_attr = $(this).attr(target[1]);
-                        if (old_attr === undefined || old_attr[0] === '#') {
+                        if (target[1] === 'srcset') {
+                            var eachLink = old_attr.split(',');
+                            var doctoredLinks = [];
+                            for (var i in eachLink) {
+                                var trimmed = eachLink[i];
+                                var sublinks = trimmed.split(' ');
+                                sublinks[0] = transformLink(sublinks[0]);
+                                doctoredLinks.push.apply(sublinks.join(' '));
+                            }
+                            $(this).attr(target[1], doctoredLinks.join(', '));
                             return;
                         }
-                        else if (old_attr[0] === '/') {
-                            if (old_attr.substring(0, 2) === '//') {
-                                old_attr = 'http:' + old_attr;
-                            }
-                            else {
-                                old_attr = urlObject.protocol + (urlObject.slashes ? '//' : '') + urlObject.hostname + (old_attr[0] === '/' ? '' : '/') + old_attr;
-                            }
+                        var new_attr = transformLink(old_attr);
+                        if (new_attr !== undefined) {
+                            $(this).attr(target[1], new_attr);
                         }
-                        else if (old_attr.substring(0, 4) !== 'http') {
-                            // We don't know what this is ... could be a link missing the http OR a call to the current directory
-                            // Nevertheless, we make our best guess
-                            var first_part = old_attr.split('/')[0];
-                            if (first_part.includes('.com') || first_part.includes('.org') || first_part.includes('.net') || first_part.includes('.edu')) {
-                                old_attr = urlObject.protocol + (urlObject.slashes ? '//' : '') + old_attr;
-                            }
-                            else {
-                                old_attr = urlLink + '/' + old_attr;
-                            }
-                        }
-                        var old_b64 = new Buffer(old_attr).toString('base64');
-                        $(this).attr(target[1], 'http://dmhacker-proxy.herokuapp.com/site/' + old_b64);
                     });
                 }
                 res.status(200).send($.html());
